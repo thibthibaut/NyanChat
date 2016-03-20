8436ab31035ab21d421b53ecf669301d23c9df89
Thibault Jouan <thibault@localhost.localdomain>
2016-03-21 00:42:44 +0100

    C est un debut, il manque tout le cote RSA maintenant

--- NyanChat/crypto/test.js
+++ NyanChat/crypto/test.js
@@ -1,4 +1,31 @@
-var crypto = require('./crypto');
+var nodeAES = require('crypto-js');
+var nodeRSA = require('node-rsa');
 
+
+
+
+
+
+var key = new nodeRSA()
+var texte = "euh, j'sais pas, genre fais comme si y'avait des trucs ecrits ici ok?";
+key.generateKeyPair(512, 65537);
+
+
+var encrypted = key.encrypt(texte, 'base64');
+var encryptedAES = nodeAES.AES.encrypt(texte, 'secret key 123');
+
+console.log('chiffre RSA: ' + encrypted);
+console.log('chiffre AES: ' + encryptedAES);
+
+var decrypted = key.decrypt(encrypted, 'utf8');
+var decryptedAES = nodeAES.AES.decrypt(encryptedAES.toString(), 'secret key 123');
+decryptedAES = decryptedAES.toString(nodeAES.enc.Utf8);
+
+console.log('dechiffre RSA: ' + decrypted);
+console.log('dechiffre AES: ' + decryptedAES);
 //Here you can test your functions
-console.log(crypto.test(5));
+//console.log(crypto.test(5));
+
+//console.log(process.argv[2]);
+
+
