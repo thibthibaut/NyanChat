9d1505f9543808bae370c1fa8f972a65c87836a6
Zios27012 <nathan.gallet@protonmail.com>
2016-03-28 11:20:37 +0200

    first fonctional chat !

--- NyanChat/client/client.js
+++ NyanChat/client/client.js
@@ -1,26 +1,34 @@
 var readline = require('readline');
-
 var io = require('socket.io-client');
 
 var nickName;
-
 var publicKey = "chauca je te laisse implementer ce truc x)";
 
-//the server should received the connection and launch 'hello world' 
-var socket = io.connect('127.0.0.1', {port: 1337});
+var socket = io.connect('http://localhost:1337');
 
 var rl = readline.createInterface(process.stdin, process.stdout); 
 
 //When the client is launched, we need to have a nick name then send it to the server
 rl.question("Please, tell me your name, and don't forget to follow the white rabbit...", function(name) {
-  
   //add easter eggs if the name is Neo :D
   nickName = name;
-
-  var message = nickName + "has enter to NyanCat";
-
-  socket.emit ('connect', {msg : message, pk: publicKey, pseudo: nickName, type:'new member'});
-
+  var message = nickName + " has enter to NyanCat";
+  socket.emit ('add user', {msg : message, pk: publicKey, pseudo: nickName});
   rl.prompt(0);
 
+});
+
+rl.on('line', function (data){
+  //add case if to allowed cmd
+  socket.emit('message', {message: data, pseudo: nickName});
+
+});
+
+//display the message for all users 
+socket.on('welcome', function(data){
+  console.log(data.message);         
+});
+
+socket.on('message', function(data){
+  console.log(data.message);
 });
--- NyanChat/server/server.js
+++ NyanChat/server/server.js
@@ -7,15 +7,30 @@ server.listen(1337, function(){
   console.log('listening');
 });
 
+var users = 0;
+
 io.on('connection', function(socket){
 
-  console.log('hello world');
- 
-  socket.on('connect', function(v){
+  users += 1;
 
-    console.log('coucou');
-    console.log(v);
-
+  socket.on('add user', function(data){
+    socket.nickname = data.pseudo;
+    ++users;
+    socket.broadcast.emit('welcome', {
+      message: data.msg + ", " + users + " users connected"
+    });
   }); 
 
+  socket.on('message', function(data){
+    socket.broadcast.emit('message', {
+      message: data.pseudo + " : " + data.message
+    });
+  });
+
+  socket.on('disconnect', function(){
+    --users;
+    socket.broadcast.emit('left', {
+      message: socket.nickname + " has left the chat"    
+     });    
+  });
 });
